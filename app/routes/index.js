07113a1730d46b8a1bd9cc7d0c428fd81229e43e
Joseph Karasek <joseph.karasek@gmail.com>
2016-03-28 11:37:13 -0700

    add announcement page to website

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
-  }
+  },
   actions: {
     destroyRental(rental) {
-      rental.destroyRocord();
+      rental.destroyRecord();
+      this.transitionTo('index');
+    },
+    save(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
       this.transitionTo('index');
     }
   }
